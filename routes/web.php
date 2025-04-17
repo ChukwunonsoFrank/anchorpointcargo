354097c43c6b73b793e2351a957322d653fbf606
Chukwunonso Frank <franknonso114@gmail.com>
2025-04-17 22:03:37 +0100

    wip

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PdfController;
+use Illuminate\Support\Facades\Artisan;
 
 /*
 |--------------------------------------------------------------------------
