d4c945aa49e6f22f795046ac653a2ec1982bdbe7
Chukwunonso Frank <franknonso114@gmail.com>
2025-04-20 11:52:50 +0100

    wip

--- resources/views/components/about-layout.blade.php
+++ resources/views/components/about-layout.blade.php
@@ -5,7 +5,6 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>AnchorPointCargo | On-Time Reliable Logistics Services.</title>
-    <meta name="description" content="Logistex - Transport & Logistics HTML Template">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
